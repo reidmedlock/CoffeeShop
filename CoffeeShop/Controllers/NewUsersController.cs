b2f3ff8bcde2efae3b35460dee41b39ec5d03eaf
reidmedlock <benmedlock19@gmail.com>
2023-07-05 21:46:46 -0400

    Add project files.

---
+++ CoffeeShop/Controllers/NewUsersController.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using CoffeeShop.Models;
+
+
+namespace CoffeeShop.Controllers
+{
+    public class NewUsersController : Controller
+    {
+        public class CoffeeShopController : Controller
+        {
+            public IActionResult Index()
+            {
+                return View();
+            }
+            public IActionResult Results(NewUser customer)
+            {
+                return View(customer);
+            }
+            public IActionResult Registration()
+            {
+                return View();
+            }
+            [HttpPost]
+            public IActionResult FormSubmit(NewUser customer)
+            {
+                return RedirectToAction("Results", customer);
+            }
+        }
+    }
+}
+
+
+
+
